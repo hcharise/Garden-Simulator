6d75a9f2598d6aae3efc7295e3f4994d4dbbf9e2
Hannah Ashton <h.charise18@gmail.com>
2024-03-17 10:29:16 -0600

    Initial Commit

---
+++ Garden Simulator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Garden Simulator
+//
+//  Created by Hannah Ashton on 3/17/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
